d716550ac18a17e59a15cb587d83fd1f9d00bf2e
KaioSantoss <kaiodaniel905@gmail.com>
2025-08-20 21:37:30 -0300

    primeiro node

---
+++ Contrução_backend/pratica/01-primeiro-projeto/index.js
@@ -0,0 +1,21 @@
+console.log("executando meu primeiro projetoem NODE")
+
+let nome = "Kaio"
+
+let sobreNome = "Santos"
+
+console.log(nome +  " " + sobreNome)
+
+let idade = 21
+
+console.log("A idade do " + nome + " é " + idade)
+
+let exercicio = 0.5
+
+let trabalho = 1.5
+
+let prova = 4.5
+
+let notaA1Calculada = exercicio + trabalho + prova
+
+console.log("NOTA A1 : " + notaA1Calculada)
